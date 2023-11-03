45fed9e55fabe7363052e72d30e3aa82e606bad8
«dgsyou» <degsleet@gmail.com>
2023-11-03 08:57:46 +0300

    выполнены пункты lesson02

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'Project name';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 73;
+let rollback = 37;
+let fullPrice = 3000;
+let adaptive = true;
 alert('любой текст в алерте');
-console.log('любой текст в консоле');
+console.log('любой текст в консоли');
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log('Стоимость верстки экранов ' + screenPrice +
+' доллара') ;
+console.log('Стоимость разработки сайта ' + fullPrice + ' долларов');
+console.log(screens.toLowerCase().split(','));
+console.log(fullPrice * (rollback / 100));
